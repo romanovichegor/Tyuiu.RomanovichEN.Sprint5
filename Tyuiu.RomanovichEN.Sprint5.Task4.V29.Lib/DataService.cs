441af46dae265892d5ced59a2b8c770928a9ae4f
Егор Романович <Egor_rom13@mail.ru>
2025-11-26 21:24:47 +0500

    Task4

---
+++ Tyuiu.RomanovichEN.Sprint5.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RomanovichEN.Sprint5.Task4.V29.Lib
+{
+    public class DataService : ISprint5Task4V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            strx = strx.Replace('.',',');
+            double x = double.Parse(strx);
+            double sinx2 = Math.Sin(Math.Pow(x, 2));
+            double res = x / (2 * x) + sinx2;
+            res = Math.Round(res, 3);
+            return res; 
+        }
+    }
+}
---
+++ Tyuiu.RomanovichEN.Sprint5.Task4.V29/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.RomanovichEN.Sprint5.Task4.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string path = @"C:\DataSprint5\InPutDataFileTask4V29.txt";
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine($"Result: {res}");
+    }
+}
